61f8e98624c472f81ff4aabf2ca5afa85abac358
Darina Danilova <darinaaibr@gmail.com>
2023-06-12 22:26:38 +0500

    «»

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
+const {
+  NOT_FOUND,
+} = require('../utils/errors');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
+
+router.use('*', (req, res) => {
+  res.status(NOT_FOUND).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 module.exports = router;
