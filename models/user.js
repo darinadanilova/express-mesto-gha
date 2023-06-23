fba10ee0b99b9de78a998c51f8ef7da8e44c06ca
Darina Danilova <darinaaibr@gmail.com>
2023-06-23 22:33:55 +0500

    «»

--- models/user.js
+++ models/user.js
@@ -6,14 +6,18 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     default: 'Жак-Ив Кусто',
-    minlength: 2,
-    maxlength: 30,
+    validate: {
+      validator: ({ length }) => length >= 2 && length <= 30,
+      message: 'Имя пользователя должно быть длиной от 2 до 30 символов',
+    },
   },
   about: {
     type: String,
     default: 'Исследователь',
-    minlength: 2,
-    maxlength: 30,
+    validate: {
+      validator: ({ length }) => length >= 2 && length <= 30,
+      message: 'Информация о пользователе должна быть длиной от 2 до 30 символов',
+    },
   },
   avatar: {
     type: String,
