de42ec31ebdff5e28dcd7b2dc2d0e0d8057d8913
Darina Danilova <darinaaibr@gmail.com>
2023-06-25 22:52:24 +0500

    «»

--- controllers/users.js
+++ controllers/users.js
@@ -58,7 +58,7 @@ const login = (req, res, next) => {
 
 const getUsers = (req, res, next) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.send(users))
     .catch(next);
 };
 
